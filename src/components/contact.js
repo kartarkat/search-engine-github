8f51c826d786c43567e2e307da61bbb3823803cc
kartarkat <kartarkat@gmail.com>
2021-12-15 11:11:49 +0530

    two

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ const contact = () => {
   return(
     <div className='ui raised very padded text container segment' style={{marignTop:'80px'}}>
     <h1>Contact us </h1>
-    <p> Hi guys, This is Karthik admin of Tamil Hacks Youtube channel, to help the Tamil Developer Community more I started this website.
+    <p> Hi guys, This is Karthikeyan admin of Tamil Hacks Youtube channel, to help the Tamil Developer Community more I started this website.
 
 check out my main channel here and my vlogs here.
 
