af07bd0b6413acdb0595f19f7d8fa5ac77a1f935
kartarkat <kartarkat@gmail.com>
2021-12-15 11:21:34 +0530

    four

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import SearchInput from './SearchInput'
-import axios from 'axios'
-import ImageList from './ImageList'
 import Navbar from './Navbar'
 import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import Home from './home'
